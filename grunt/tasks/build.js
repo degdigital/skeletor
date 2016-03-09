40aa8f3a7cf1b2dd7e869dca1b00a765a0348c8a
Ryan Heap <rheap@degdigital.com>
2016-03-08 22:09:23 -0600

    added sub-task targets to build and export tasks

--- grunt/tasks/build.js
+++ grunt/tasks/build.js
@@ -3,12 +3,12 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('build', function() {
 		grunt.config('activeTheme', this.data);
 		
-   		grunt.task.run('build-patterns');
-		grunt.task.run('build-css');
-		grunt.task.run('build-js');
-		grunt.task.run('build-images');
-		grunt.task.run('build-fonts');
-		grunt.task.run('build-api');
+   		grunt.task.run('build-patterns:' + this.target);
+		grunt.task.run('build-css:' + this.target);
+		grunt.task.run('build-js:' + this.target);;
+		grunt.task.run('build-images:' + this.target);
+		grunt.task.run('build-fonts:' + this.target);
+		grunt.task.run('build-api:' + this.target);
 	});
 
 }
--- grunt/tasks/export.js
+++ grunt/tasks/export.js
@@ -3,12 +3,12 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('export', function() {
 		grunt.config('activeTheme', this.data);
 
-		grunt.task.run('export-patterns');
-		grunt.task.run('export-css');
-		grunt.task.run('export-js');
-		grunt.task.run('export-images');
-		grunt.task.run('export-fonts');
-		grunt.task.run('export-api');
+		grunt.task.run('export-patterns:' + this.target);
+		grunt.task.run('export-css:' + this.target);
+		grunt.task.run('export-js:' + this.target);
+		grunt.task.run('export-images:' + this.target);;
+		grunt.task.run('export-fonts:' + this.target);
+		grunt.task.run('export-api:' + this.target);
 	});
 
 }
