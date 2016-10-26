8cb1d34e37906b0d5ee81ae79aa026a1e3622303
Ryan Heap <heaper@gmail.com>
2016-10-26 15:13:21 -0500

    Fixed missing activeTheme variable bug in export-patterns

--- grunt/tasks/patterns/export-patterns.js
+++ grunt/tasks/patterns/export-patterns.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 	grunt.registerMultiTask('export-patterns', function() {
 		grunt.config('activeTheme', this.data);
+		var activeTheme = grunt.config('activeTheme');
 
 		if(activeTheme.source.assetPaths.patterns && activeTheme.export.assetPaths.patterns) {
 			grunt.task.run('build-patterns:' + this.target);
